29437ebee308740009a0b91177087abdf76428a5
Yashvardhan Singh Bhadoria <yashvardhans016@gmail.com>
2024-08-31 00:24:55 +0530

    modified:   requirements.txt and app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def predict():
     return render_template('services.html', prediction_text="The price of the crop will be: " + "Rs. " + str(round(prediction_unscaled_int)))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
